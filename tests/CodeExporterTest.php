c4ed47e18c965e501c3c271f394208d79d06423d
Karsten Frohwein <karsten.frohwein@w-f.ch>
2019-09-07 11:46:40 +0200

    Added test for the ini settings export. Checking if "display_errors" is set to true.

--- tests/CodeExporterTest.php
+++ tests/CodeExporterTest.php
@@ -32,4 +32,26 @@ final class CodeExporterTest extends TestCase
             $exporter->globalVariables($snapshot)
         );
     }
+
+    /**
+     * @runInSeparateProcess
+     */
+    public function testCanExportIniSettingsToCode(): void
+    {
+        $iniSettingName = 'display_errors';
+        ini_set($iniSettingName, '1');
+        $iniValue = ini_get($iniSettingName);
+
+        $snapshot = new Snapshot(null, false, false, false, false, false, false, false, true, false);
+
+        $exporter = new CodeExporter;
+        $export = $exporter->iniSettings($snapshot);
+
+        $pattern = "/@ini_set\(\'$iniSettingName\', \'$iniValue\'\);/";
+
+        $this->assertRegExp(
+            $pattern,
+            $export
+        );
+    }
 }
