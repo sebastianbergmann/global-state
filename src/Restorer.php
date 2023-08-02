66783ce213de415b451b904bfef9dda0cf9aeae0
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-08-02 11:23:32 +0200

    Backport c57acacdba5b303b5db84eff6d4067be108af911

--- src/Restorer.php
+++ src/Restorer.php
@@ -74,7 +74,7 @@ class Restorer
             foreach ($staticAttributes as $name => $value) {
                 $reflector = new \ReflectionProperty($className, $name);
                 $reflector->setAccessible(true);
-                $reflector->setValue($value);
+                $reflector->setValue(null, $value);
             }
         }
 
@@ -98,7 +98,7 @@ class Restorer
                 }
 
                 $attribute->setAccessible(true);
-                $attribute->setValue($defaults[$name]);
+                $attribute->setValue(null, $defaults[$name]);
             }
         }
     }
